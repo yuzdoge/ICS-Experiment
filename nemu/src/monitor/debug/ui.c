b8b43f8594c562eb4b8a2bcf798b16d47e3783e1
tracer-ics2020 <tracer@njuics.org>
2021-04-10 17:59:15 +0800

    >  compile 181420000    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: d56fa518b5c34852bc98d04d582ca791            Boot ID: 34591d9ef7bd4ad7b0a18a32a79e1d90     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  17:59:15 up  5:01,  2 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -65,7 +65,7 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-  { "si", "Step program until N instructions\n\t\tUsage: si [N]", cmd_si },
+  { "si", "Step program until N instructions\n\tUsage: si [N]", cmd_si },
   /* TODO: Add more commands */
 
 };
