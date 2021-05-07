1131e9e9669027e81493d751354db7633dada73e
tracer-ics2020 <tracer@njuics.org>
2021-05-07 13:33:37 +0800

    >  compile 181420000    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: d56fa518b5c34852bc98d04d582ca791            Boot ID: d6ecff03cc1443febd81b49b1d2a374a     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  13:33:37 up  2:57,  1 user,  load average: 0.10, 0.03, 0.01

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[]) {
 #ifdef TEST_EXPR
 #define BUFSIZE (100 + 20) 
 	char buf[BUFSIZE];
-	FILE *fp = fopen("../tools/gen-expr/input", "r");
+	FILE *fp = fopen("input", "r");
 	perror("Open error");
 	assert(fp);
 	while (fgets(buf, BUFSIZE, fp)){
