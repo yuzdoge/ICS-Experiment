a5d1a66a2690edb0ca46930af8ddeb80905d980e
tracer-ics2020 <tracer@njuics.org>
2021-06-02 21:54:53 +0800

    >  compile 181420000    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: d56fa518b5c34852bc98d04d582ca791            Boot ID: cef150f0d9ff4e188780eaf2ca4cf3a2     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  21:54:53 up 37 min,  1 user,  load average: 0.07, 0.02, 0.00

--- nemu/src/isa/riscv32/exec/control.h
+++ nemu/src/isa/riscv32/exec/control.h
@@ -11,7 +11,7 @@ static inline def_EHelper(jalr){
   switch (s->isa.instr.i.funct3){
     case 0b000:
 	  rtl_addi(s, s0, dsrc1, cpu.pc + id_src2->imm);
-	  rtl_andi(s, s0, s0, ~(-1));
+	  rtl_andi(s, s0, s0, ~(0b1));
 	  rtl_jr(s, s0);
 	  rtl_li(s, ddest, s->seq_pc);
       print_Dop(id_src1->str, OP_STR_SIZE, "%d(%s)", id_src2->imm, reg_name(id_src1->reg, 4));
