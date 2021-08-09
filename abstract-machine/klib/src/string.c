bf133dd1c6ff9cdf92cf1805aa65ba680eb38d85
tracer-ics2020 <tracer@njuics.org>
2021-08-09 11:15:46 +0800

    >  run 181420000    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: d56fa518b5c34852bc98d04d582ca791            Boot ID: 1e49aa3b12324ef3a93b76fa5b699385     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  11:15:47 up 20 min,  3 users,  load average: 0.08, 0.03, 0.03

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -4,31 +4,59 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  return 0;
+  size_t cnt = 0;
+  while (*s) cnt++; 
+  return cnt;
 }
 
 char *strcpy(char* dst,const char* src) {
-  return NULL;
+  for (char *p = dst; ; p++, src++) {
+    *p = *src;
+	if (*src == '\0') break;
+  }
+  return dst;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-  return NULL;
+  size_t i;
+  for (i = 0; i < n && src[i] != '\0'; i++) 
+    dst[i] = src[i];
+  for (; i < n; n++) dst[i] = '\0';
+  return dst; 
 }
 
 char* strcat(char* dst, const char* src) {
-  return NULL;
+  char *p; 
+  for (p = dst; *p; p++);
+  for ( ; ; p++, src++) {
+    *p = *src;
+	if (*src == '\0') break;
+  }
+  return dst;
 }
 
 int strcmp(const char* s1, const char* s2) {
-  return 0;
+/*s1_len > s2_len <-> s1[s2_len] > s2[s2_len]
+ *s1_len < s2_len <-> s1[s1_len] < s2[s1_len] 
+ */
+  for (; *s1 && (*s1 - *s2) == 0; s1++, s2++);
+  return (*s1 - *s2);
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
-  return 0;
+/*the condition must be n > 1, not n > 0 
+ *if n > s1_len, it depends on *s1 and (*s1 - *s2)
+ *if n <= s1_len, it depends on n and *(s1 - *s2)
+ */
+  if (n == 0) return 0;
+  for (; n > 1 && *s1 && (*s1 - *s2) == 0; s1++, s2++, n--);
+  return (*s1 - *s2);
 }
 
 void* memset(void* v,int c,size_t n) {
-  return NULL;
+  char *tmp = (char*)v;
+  while (n--) tmp[n] = c; //n = n -1 right after judgement.
+  return v;
 }
 
 void* memmove(void* dst,const void* src,size_t n) {
@@ -40,7 +68,11 @@ void* memcpy(void* out, const void* in, size_t n) {
 }
 
 int memcmp(const void* s1, const void* s2, size_t n) {
-  return 0;
+  if (n == 0) return 0;
+  unsigned char *s1_ = (unsigned char *)s1;
+  unsigned char *s2_ = (unsigned char *)s2;
+  for ( ; n > 1 && (*s1_ - *s2_) == 0; s1_++, s2_++, n--);
+  return (*s1_ - *s2_); 
 }
 
 #endif
