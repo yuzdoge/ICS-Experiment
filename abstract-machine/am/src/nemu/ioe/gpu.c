38a77ba1774f704552d29d1314be935f4e4e920c
tracer-ics2020 <tracer@njuics.org>
2021-09-21 23:13:05 +0800

    >  compile 181420000    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: d56fa518b5c34852bc98d04d582ca791            Boot ID: c1955415c69d422d9b1b578b14922f12     Virtualization: vmware   Operating System: Debian GNU/Linux 10 (buster)             Kernel: Linux 4.19.0-14-amd64       Architecture: x86-64  23:13:05 up 32 min,  2 users,  load average: 0.52, 0.43, 0.18

--- abstract-machine/am/src/nemu/ioe/gpu.c
+++ abstract-machine/am/src/nemu/ioe/gpu.c
@@ -4,12 +4,20 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
+  int i;
+  uint32_t rect = inl(VGACTL_ADDR);
+  int w = rect >> 16; 
+  int h = rect & 0x0000ffff; 
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i++) fb[i] = i;
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  uint32_t rect = inl(VGACTL_ADDR);
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
+    .width = rect >> 16, .height = rect & 0x0000ffff,
     .vmemsz = 0
   };
 }
